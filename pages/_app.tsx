f1cb76e9be4836038a31f1adea71e8a2c9a01fb6
Alypher Mansano <alypher@gmail.com>
2023-06-19 15:10:16 -0300

    added ga tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import 'nextra-theme-blog/style.css'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import '../styles/main.css'
+import Script from 'next/script'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,6 +22,21 @@ export default function App({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
         />
       </Head>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-WQHBDQMS5D"/>   
+      <Script
+        id='google-analytics'
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-WQHBDQMS5D', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+      />
       <Component {...pageProps} />
     </>
   )
